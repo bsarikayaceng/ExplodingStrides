de6bb7fec6875790bb585cee43cb0900318de689
Büşra SARIKAYA <81385058+bsarikayaceng@users.noreply.github.com>
2023-08-07 10:32:57 +0300

    game manager instance made

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 }
