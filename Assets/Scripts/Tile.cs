bdfcf2d2dda41a41df01d2abc42eb0fa2c8c29a8
Büşra SARIKAYA <bsrsrky.n@gmail.com>
2023-08-28 18:09:09 +0300

    Zero-Clearing Added

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -21,8 +21,19 @@ public class Tile : MonoBehaviour
 
     private bool isClicked=false;
 
+    private bool isOpen = false;
+
     public static Tile Instance { get; private set; }
 
+    private TileState _tileState;
+
+    public TileState TileState
+    {
+        get { return _tileState; }
+        set { _tileState = value; }
+    }
+
+    public Transform rabbitTransform; // Tavşanin Transformu
 
     private void Awake()
     {
@@ -44,6 +55,9 @@ public class Tile : MonoBehaviour
     }
     public void OnTileClick()
     {
+        //TestTouch.Instance.SetTargetTile(this);
+        //Vector3 targetPosition = new Vector3(x, y, 0); // Tile'ın koordinatlarını kullanarak hedef pozisyonu oluştur
+        //TestTouch.SetTargetPosition(targetPosition);
         if (isClicked) return;
         GridManager.Instance.TileClickCount();
         if (gridManager.IsGameOver()||gridManager.IsGameWin())
@@ -62,10 +76,12 @@ public class Tile : MonoBehaviour
         else
         {
             grass.SetActive(false);
-
+            //Vector3 clickedPosition = new Vector3(x, y, rabbitTransform.position.z); // Tıkladığınız karenin konumu
+           // rabbitTransform.position = clickedPosition; // Tavşan karakterin pozisyonunu güncelle
             if (neighborMineCount == 0)
             {
                 mineCountText.text = neighborMineCount.ToString();
+                OpenZeroTilesRecursively();
             }
             else
             {
@@ -77,7 +93,61 @@ public class Tile : MonoBehaviour
             Debug.Log($"Tıklanan karenin koordinatları: X = {x}, Y = {y}");
         }
     }
-    
+
+
+
+    public void OpenZeroTilesRecursively()
+    {
+        if (TileState == TileState.Open)
+            return;
+
+        TileState = TileState.Open;
+        grass.SetActive(false);
+        mineCountText.gameObject.SetActive(true);
+
+        List<Tile> neighbours = GetNeighbours(x, y);
+
+        foreach (var neighbour in neighbours)
+        {
+            if (neighbour.TileState != TileState.Open)
+            {
+                int neighborMineCount = neighbour.tileController.CalculateNeighborMineCount(neighbour.x, neighbour.y);
+                neighbour.mineCountText.text = neighborMineCount.ToString();
+
+                if (neighborMineCount == 0)
+                {
+                    neighbour.OpenZeroTilesRecursively();
+                }
+            }
+        }
+    }
+
+
+
+    private List<Tile> GetNeighbours(int x, int y)
+    {
+        List<Tile> neighbours = new List<Tile>();
+
+        for (int i = x - 1; i <= x + 1; i++)
+        {
+            for (int j = y - 1; j <= y + 1; j++)
+            {
+                if (i == x && j == y)
+                    continue;
+
+                if (i >= 0 && i < GridManager.Instance.GetGridSizeX && j >= 0 && j < GridManager.Instance.GetGridSizeY)
+                {
+                    neighbours.Add(GridManager.Instance.GetTileAtPosition(i, j));
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
+
+
+
     public bool IsClicked() => isClicked;
 }
 
@@ -88,3 +158,9 @@ public enum UnitState
      Mine
 }
 
+public enum TileState
+{
+    Closed,
+    Open,
+    Revealed
+}
